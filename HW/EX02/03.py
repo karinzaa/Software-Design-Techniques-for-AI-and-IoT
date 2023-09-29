835b95c5379f68244fbb48f72a09ccf3c75dc4ea
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-09-29 10:14:28 +0700

    Changes to be committed:Add HW/EX02/03.py 	new file:   HW/EX02/03.py

---
+++ HW/EX02/03.py
@@ -0,0 +1,37 @@
+import sys
+
+def merge(array, left, mid, right):
+    temp = []
+    i = left
+    j = mid + 1
+    while i <= mid and j <= right:
+        if array[i] < array[j]:
+            temp.append(array[i])
+            i += 1
+        else:
+            temp.append(array[j])
+            j += 1
+    while i <= mid:
+        temp.append(array[i])
+        i += 1
+    while j <= right:
+        temp.append(array[j])
+        j += 1
+    for i in range(left, right + 1):
+        array[i] = temp[i - left]
+
+def merge_sort(array, left, right):
+
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(array, left, mid)
+        merge_sort(array, mid + 1, right)
+        merge(array, left, mid, right)
+
+def main():
+    array = list(map(int, sys.argv[1:]))
+    merge_sort(array, 0, len(array) - 1)
+    print(array)
+
+if __name__ == '__main__':
+    main()
