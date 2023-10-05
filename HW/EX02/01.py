4e2a58b1c9dfa3e973bc285da2c71cd47c0cefa3
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-10-05 15:32:45 +0700

    Changes to be committed: Fix typo 	modified:   HW/EX02/01.py 	modified:   HW/EX03/01-07.ipynb

--- HW/EX02/01.py
+++ HW/EX02/01.py
@@ -4,6 +4,6 @@ def sort_words(words):
     return sorted(words)
 
 if __name__ == "__main__":
-    words = sys.argv[1:]  # Get words from arguments
+    words = sys.argv[1:]
     sorted_words = sort_words(words)
     print(' '.join(sorted_words))
